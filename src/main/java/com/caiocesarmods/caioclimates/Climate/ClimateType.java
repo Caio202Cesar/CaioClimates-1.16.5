c519a0f6002c46e440031f795c7961dc145c786c
Caio202Cesar <caiodnobre@gmail.com>
2025-05-02 07:53:05 -0300

    ...

--- src/main/java/com/caiocesarmods/caioclimates/Climate/ClimateType.java
+++ src/main/java/com/caiocesarmods/caioclimates/Climate/ClimateType.java
@@ -2,6 +2,8 @@ package com.caiocesarmods.caioclimates.Climate;
 
 import com.caiocesarmods.caioclimates.Seasons.Season;
 
+import java.util.Objects;
+
 public enum ClimateType {
     MEDITERRANEAN_HOT(new float[]{0.7F, 1.2F, 0.6F, 0.4F}, new boolean[] {false, false, true, true}),
     MARITIME_MEDITERRANEAN(new float[]{0.6F, 0.7F, 0.6F, 0.4F}, new boolean[] {false, false, true, true}),
@@ -23,8 +25,8 @@ public enum ClimateType {
     TAIGA(new float[]{0.12F, 0.3F, 0.2F, -0.12F}, new boolean[] {true, true, true, true}),
     SNOWY_TAIGA(new float[]{-0.3F, 0.25F, -0.2F, -0.4F}, new boolean[] {true, true, true, true}),
     ICE_CAP(new float[]{0.0F, 0.0F, 0.0F, 0.0F}, new boolean[] {true, true, true, true}),
-    TROPICAL_DESERT(new float[]{1.2F, 1.2F, 1.2F, 1.2F}, new boolean[] {false, false, false, false}),
-    SUBTROPICAL_DESERT(new float[]{0.8F, 1.2F, 0.9F, 0.4F}, new boolean[] {false, false, false, false}),
+    TROPICAL_DESERT(new float[]{1.2F, 2.0F, 1.2F, 1.2F}, new boolean[] {false, false, false, false}),
+    SUBTROPICAL_DESERT(new float[]{0.8F, 2.0F, 1.2F, 0.4F}, new boolean[] {false, false, false, false}),
     MARITIME_SUBTROPICAL_DESERT(new float[]{0.6F, 0.7F, 0.6F, 0.4F}, new boolean[] {false, false, false, false});
 
     private final float[] seasonalTemperatures;
@@ -37,6 +39,16 @@ public enum ClimateType {
 
     public float getTemperature(Season season) {
         return this.seasonalTemperatures[season.ordinal()];
+    }
+
+    public enum Weather {
+        CLEAR,
+        RAIN,
+        THUNDERING;
+
+        public static Weather getWeatherByString(String s) {
+            return Objects.equals(s, "thundering") ? THUNDERING : Objects.equals(s, "rain") ? RAIN : CLEAR;
+        }
     }
 
     public boolean hasRain(Season season) {
