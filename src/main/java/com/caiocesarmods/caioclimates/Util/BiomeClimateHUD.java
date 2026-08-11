17cbdf8347d7544f6169f1ead19b07588a52d6ef
Caio202Cesar <caiodnobre@gmail.com>
2026-08-11 20:40:24 -0300

    Pilot

--- src/main/java/com/caiocesarmods/caioclimates/Util/BiomeClimateHUD.java
+++ src/main/java/com/caiocesarmods/caioclimates/Util/BiomeClimateHUD.java
@@ -2,11 +2,14 @@ package com.caiocesarmods.caioclimates.Util;
 
 import com.caiocesarmods.caioclimates.Climate.ClimateDomain;
 import com.caiocesarmods.caioclimates.Climate.ClimateDomainRegistry;
+import com.caiocesarmods.caioclimates.Climate.SummerHeat.SummerHeat;
+import com.caiocesarmods.caioclimates.Climate.SummerHeat.SummerHeatHelper;
 import com.caiocesarmods.caioclimates.Seasons.Season;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -22,9 +25,17 @@ public class BiomeClimateHUD {
         World world = player.world;
         long dayTime = world.getDayTime();
         BlockPos pos = player.getPosition();
+        Biome biome = world.getBiome(pos);
 
         String season = Season.getSeason(dayTime);
         ClimateDomain climate = ClimateDomainRegistry.get(world, pos);
+
+        SummerHeat summerHeat = SummerHeat.fromTemperature(
+                SummerHeatHelper.get(world, pos)
+        );
+
+        float baseTemp = biome.getTemperature(pos);
+        float downfall = biome.getDownfall();
 
         int lineHeight = 10;
         int margin = 40;
@@ -33,6 +44,15 @@ public class BiomeClimateHUD {
 
         // Two lines, starting from the bottom.
         int y = screenHeight - margin - (lineHeight * 2);
+
+        drawText(mc, event, "Summer Heat: " + summerHeat, 10, y);
+        y += lineHeight;
+
+        drawText(mc, event, String.format("Biome Base Temperature: %.2f", baseTemp), 10, y);
+        y += lineHeight;
+
+        drawText(mc, event, String.format("Downfall: %.2f", downfall), 10, y);
+        y += lineHeight;
 
         drawText(mc, event, "Global Season: " + season, 10, y);
         y += lineHeight;
