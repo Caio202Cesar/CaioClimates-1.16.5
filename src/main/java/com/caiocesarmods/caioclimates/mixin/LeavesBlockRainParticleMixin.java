349d5135b03067aae3fec12c679fd540f928607e
Caio202Cesar <caiodnobre@gmail.com>
2026-08-12 13:50:39 -0300

    Pilot

---
+++ src/main/java/com/caiocesarmods/caioclimates/mixin/LeavesBlockRainParticleMixin.java
@@ -0,0 +1,38 @@
+package com.caiocesarmods.caioclimates.mixin;
+
+import com.caiocesarmods.caioclimates.Climate.Winter.SnowfallHandler;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(LeavesBlock.class)
+public abstract class LeavesBlockRainParticleMixin {
+
+    @Redirect(
+            method = "animateTick",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/World;isRainingAt(Lnet/minecraft/util/math/BlockPos;)Z"
+            )
+    )
+    private boolean caio$useLocalPrecipitation(
+            World world,
+            BlockPos pos
+    ) {
+        if (!world.isRemote) {
+            return world.isRainingAt(pos);
+        }
+
+        Biome biome = world.getBiome(pos);
+
+        if (SnowfallHandler.shouldSnow(biome, pos, world)) {
+            return false;
+        }
+
+        return world.isRainingAt(pos);
+    }
+}
--- src/main/java/com/caiocesarmods/caioclimates/mixin/WorldRendererMixin.java
+++ src/main/java/com/caiocesarmods/caioclimates/mixin/WorldRendererMixin.java
@@ -30,9 +30,7 @@ import net.minecraft.world.gen.Heightmap;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Random;
