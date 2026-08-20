200e116a4e9143880872e3b8fcf4b8ecc47b1eb3
Caio202Cesar <caiodnobre@gmail.com>
2026-08-20 20:46:16 -0300

    Snow now melts in warmer seasons, not only if it is raining.

--- src/main/java/com/caiocesarmods/caioclimates/Climate/Winter/SnowMeltHandler.java
+++ src/main/java/com/caiocesarmods/caioclimates/Climate/Winter/SnowMeltHandler.java
@@ -1,20 +1,40 @@
 package com.caiocesarmods.caioclimates.Climate.Winter;
 
+import com.caiocesarmods.caioclimates.Seasons.Season;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.server.ServerWorld;
 
 public class SnowMeltHandler {
 
-    public static boolean shouldMelt(
-            ServerWorld world,
-            BlockPos pos
-    ) {
-        if (!world.isRaining()) {
-            return false;
-        }
+    public static boolean shouldMelt(ServerWorld world, BlockPos pos) {
+        String currentSeason = Season.getSeason(world.getDayTime());
 
         Biome biome = world.getBiome(pos);
+        float temperature = biome.getTemperature(pos);
+
+        boolean iceCapClimate = temperature < 0.0F;
+        boolean taigaClimate = temperature < 0.4F;
+        boolean mesothermalClimate = temperature >= 0.7F;
+
+        //If it is not raining
+        if (!world.isRaining()) {
+            if ("SUMMER".equals(currentSeason) && !iceCapClimate) {
+                return true;
+            }
+            else if ("SPRING".equals(currentSeason) && !iceCapClimate && !taigaClimate) {
+                return true;
+            }
+            else if ("FALL".equals(currentSeason) && !iceCapClimate && !taigaClimate) {
+                return true;
+            }
+
+            else if ("WINTER".equals(currentSeason) && mesothermalClimate) {
+                return true;
+            }
+
+            return false;
+        }
 
         // If precipitation at this location is snow,
         // existing snow should remain.
